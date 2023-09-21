d35cadd402614feb2d2f46a31414dc1d44f9a924
georgeolivares <88570786+georgeolivares@users.noreply.github.com>
2023-09-20 19:32:17 -0600

    2nd jupyter notebook

--- SQLQuery Dataset in MS SQL.sql
+++ SQLQuery Dataset in MS SQL.sql
@@ -1,7 +1,33 @@
 CREATE DATABASE businessERP;
 GO
 
-CREATE TABLE company(companyID INT, company_name CHAR(150));
+CREATE TABLE company(companyID INT, company_name CHAR(150), created_date DATETIME);
 GO
 
-DROP TABLE company;
+
+INSERT INTO company(companyID, company_name, created_date) VALUES(1, 
+'Livepath', CONVERT(datetime, '28-03-23 00:00:00 AM', 5))
+GO
+
+SELECT * FROM company;
+GO
+
+CREATE TABLE employees(employee_id INT, first_name CHAR(100), 
+last_name CHAR(200), email CHAR(200), related_company INT)
+GO
+
+SELECT * FROM employees;
+GO
+
+BULK INSERT employees
+FROM 'C:\Users\jorge\OneDrive\Escritorio\Backup OneDrive\DatasetSQLs\employees.csv'
+WITH (
+		FORMAT = 'CSV',
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		ROWTERMINATOR = '\n'
+		)
+GO
+
+
+DROP TABLE employees;
